db0fc013c65a6609a9d09b32a64f2d2002265b84
Snowball_233 <wangrb.qq@outlook.com>
2024-09-17 20:44:40 +0800

    Create sb.h

---
+++ sb.h
@@ -0,0 +1,27 @@
+#ifndef SB_H
+#define SB_H
+
+// words
+#define mian main
+#define incude include
+#define stadin std::cin
+#define stadout std::cout
+#define tmeplate template
+#define ture true
+#define fasle false
+#define retrun return
+#define constrauctor constructor
+#define destrouctor destructor
+
+// symbols
+#define “ "
+#define ” "
+#define ‘ '
+#define ’ '
+#define ， ,
+#define （ (
+#define ）)
+#define ； ;
+#define ： :
+
+#endif
